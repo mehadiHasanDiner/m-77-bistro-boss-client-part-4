81713b4cde906edb3c28dca9f6a2de500523e9a1
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-11 02:11:11 +0600

    done

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,17 +3,20 @@ import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 
 const useCart = () => {
-    const {user} = useContext(AuthContext);
-    const token = localStorage.getItem('access-token');
+  const { user } = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
 
-    const { refetch, data: cart=[] } = useQuery({
-      queryKey: ["carts", user?.email],
-      queryFn: async () => {
-    const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {headers:{authorization: `bearer ${token}`}})
-    return res.json()
-  },
-    });
-    return [cart, refetch];
+  const { refetch, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/carts?email=${user?.email}`,
+        { headers: { authorization: `bearer ${token}` } }
+      );
+      return res.json();
+    },
+  });
+  return [cart, refetch];
 };
 
 export default useCart;

4bd9de4968257f898c8935a157505d5e8bf9a9d6
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-17 07:36:51 +0600

    video 79

--- _make_secure.js
+++ _make_secure.js
@@ -5,12 +5,12 @@
  * 1. do not show the link to them who should not see it
  * 2. only show the person/types of user who should see it
  * 3. do not allow to visit the link by typing on the url
- * 
+ *
  * ---------------------------
  *      TO SEND DATA
  * ---------------------------
  * 1. verify jwt token
  * 2. if it is an admin activity. make sure only admin user is posting data by using verify admin.
- * 
- * 
- * */ 
+ *
+ *
+ * */

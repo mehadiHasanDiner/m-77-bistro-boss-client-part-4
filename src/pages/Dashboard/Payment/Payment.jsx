129f73c9537eaf8076f2d9134a32c7f5ef0a3b28
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-26 07:21:06 +0600

    checkout form of payment method done

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,4 +1,10 @@
+import { Elements } from "@stripe/react-stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+
+// TODO: provide a publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
   return (
@@ -8,6 +14,9 @@ const Payment = () => {
         heading="Payment"
       ></SectionTitle>
       <h2 className="text-3xl">Taka taka o taka tumi koyi</h2>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
     </div>
   );
 };
